a0015e9eefb1e475ed5e7072855c395558764fb8
Michael Tambornino <michael.tambornino@gmail.com>
2017-04-19 09:50:43 -0500

    knex/bookshelf setup

--- db/database.js
+++ db/database.js
@@ -1,4 +1,13 @@
+'use strict'
+
 // <Setup the configuration module for knex and bookshelf>
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../knexfile')[environmet]
+const knex = require('knex')(config)
+const bookshelf = require('bookshelf')(knex)
+
+// avoid circular dependencies
+bookshelf.plugin(registry)
 
 // I have already run 'knex init' at the root of this project to create the knexfile.js.
 // The music history database lives right here as a document in this project, so note
