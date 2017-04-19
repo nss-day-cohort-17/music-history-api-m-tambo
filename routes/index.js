a7a7544bc5591d052944bd42bc0918f46698345f
Michael Tambornino <michael.tambornino@gmail.com>
2017-04-19 12:52:51 -0500

    defining routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 'use strict'
 
 // <require express Router and require the songs.js router file>
+const Router = require('express')
+const router = Router()
 
-
+router.use(require('./songs.js'))
+router.use(require('./root.js'))
 // this is an example of a root document. It returns a json representation of
 // all of our endpoints. (Just one endpoint exists in this tiny example app)
-router.get('/', function(req, res) {
-  res.json({
-    "title": "The Music History API",
-    "songs": "http://localhost:3000/api/v1/songs",
-  });
-});
+
+
+
+module.exports = router
